b8328fc26fc8393cdc05724549363af5d2a5c8d1
Oliver Schall <oliverschall22@googlemail.com>
2015-01-19 00:52:40 +0100

    Added TRIANGLE_SUBDIVISION für IsoSpheres.

--- JAwesomeEngine/src/shape/IsoSphere.java
+++ JAwesomeEngine/src/shape/IsoSphere.java
@@ -13,19 +13,22 @@ import vector.Vector3f;
 public class IsoSphere extends ShapedObject implements SphereStructure {
 	float radius;
 	
-	public IsoSphere(float x, float y, float z, float radius, int subdivisions) {
+	public final static int POINT_SUBDIVISION = 0;
+	public final static int TRIANGLE_SUBDIVISION = 1;
+	
+	public IsoSphere(float x, float y, float z, float radius, int divisionmethod, int subdivisions) {
 		super();
 		translateTo(x, y, z);
-		init(radius, subdivisions);
+		init(radius, divisionmethod, subdivisions);
 	}
 	
-	public IsoSphere(Vector3f pos, float radius, int subdivisions) {
+	public IsoSphere(Vector3f pos, float radius, int divisionmethod, int subdivisions) {
 		super();
 		translateTo(pos);
-		init(radius, subdivisions);
+		init(radius, divisionmethod, subdivisions);
 	}
 	
-	private void init(float radius, int subdivisions) {
+	private void init(float radius, int divisionmethod, int subdivisions) {
 		this.radius = radius;
 		List<Vector3f> vertices = new ArrayList<Vector3f>();
 		List<Integer[]> faces = new ArrayList<Integer[]>();
@@ -40,38 +43,70 @@ public class IsoSphere extends ShapedObject implements SphereStructure {
 		faces.add(new Integer[] {0, 2, 3, 2, 1, 2});
 		faces.add(new Integer[] {0, 3, 1, 3, 2, 3});
 		
-		int vertexcount = 3;
 		for(int i = 0; i < subdivisions; i++) {
 			List<Integer[]> currfaces = new ArrayList<Integer[]>(faces);
+			switch(divisionmethod) {
+			case POINT_SUBDIVISION:
+				for(int j = currfaces.size()-1; j >= 0; j--) {
+					Integer[] f = currfaces.get(j);
+
+					Vector3f a = vertices.get(f[0]);
+					Vector3f b = vertices.get(f[2]);
+					Vector3f c = vertices.get(f[4]);
+					
+					Vector3f d = VecMath.setScale(VecMath.addition(VecMath.addition(a, b), c), radius);
+					int vc = vertices.size();
+					vertices.add(d);
+					
+					faces.remove(j);
+					faces.add(new Integer[] {f[0], 0, f[2], 0, vc, 0});
+					faces.add(new Integer[] {f[0], 0, vc, 0, f[4], 0});
+					faces.add(new Integer[] {vc, 0, f[2], 0, f[4], 0});
+				}
+			break;
+			case TRIANGLE_SUBDIVISION:
+			for(Integer[] f : currfaces) {
+				Vector3f v = getCenter(vertices.get(f[2]), vertices.get(f[4]));
+				if(!vertices.contains(v)) {
+					vertices.add(v);
+				}
+				v = getCenter(vertices.get(f[0]), vertices.get(f[4]));
+				if(!vertices.contains(v)) {
+					vertices.add(v);
+				}
+			}
+			
 			for(int j = currfaces.size()-1; j >= 0; j--) {
 				Integer[] f = currfaces.get(j);
-				Vector3f a = vertices.get(f[0]);
-				Vector3f b = vertices.get(f[2]);
-				Vector3f c = vertices.get(f[4]);
-				
-				//Triangles werden falsch subdivided, statt point in die mitte zu setzen, point an jede kante setzen
-				//und 4 statt 3 triangles generieren. (in anderer schleife um redundanzen zu vermeiden)
-				//siehe: http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
-				Vector3f d = VecMath.setScale(VecMath.addition(VecMath.addition(a, b), c), radius);
-//				Vector3f d = VecMath.scale(VecMath.addition(VecMath.addition(a, b), c), 1/3f);
-				vertexcount++;
-				vertices.add(d);
+
+				int a = vertices.indexOf(getCenter(vertices.get(f[0]), vertices.get(f[2])));
+				int b = vertices.indexOf(getCenter(vertices.get(f[4]), vertices.get(f[0])));
+				int c = vertices.indexOf(getCenter(vertices.get(f[2]), vertices.get(f[4])));
 				
 				faces.remove(j);
-				faces.add(new Integer[] {f[4], f[0], vertexcount, f[0], f[2], f[0]});
-				faces.add(new Integer[] {f[0], f[4], f[2], f[4], vertexcount, f[4]});
-				faces.add(new Integer[] {f[0], f[2], vertexcount, f[2], f[4], f[2]});
+				faces.add(new Integer[] {f[0], 0, a, 0, b, 0});
+				faces.add(new Integer[] {b, 0, c, 0, f[4], 0});
+				faces.add(new Integer[] {a, 0, f[2], 0, c, 0});
+				faces.add(new Integer[] {a, 0, c, 0, b, 0});
+			}
+			break;
 			}
 		}
-		System.out.println(faces.size());
 		
 		for(Vector3f v : vertices)
 			addVertex(v, Color.GRAY, new Vector2f(0, 0), VecMath.normalize(v));
+		vertices.clear();
+//		edges.clear();
 		
 		for(Integer[] f : faces)
 			addTriangle(f[0], f[1], f[2], f[3], f[4], f[5]);
+		faces.clear();
 		
 		this.prerender();
+	}
+	
+	private Vector3f getCenter(Vector3f a, Vector3f b) {
+		return VecMath.setScale(VecMath.addition(a, b), radius);
 	}
 
 	@Override
