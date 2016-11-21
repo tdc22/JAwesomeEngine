12991d7c18f1b8f4c550fad59351bcf9e9ffef5c
Oliver <oliverschall22@googlemail.com>
2016-11-21 09:18:44 +0100

    Started implementing 2d raycast hitpoint calculation

--- JAwesomePhysics/src/narrowphase/SupportRaycast2.java
+++ JAwesomePhysics/src/narrowphase/SupportRaycast2.java
@@ -1,22 +1,27 @@
 package narrowphase;
 
+import math.VecMath;
 import objects.Ray;
 import objects.SupportMap;
 import vector.Vector2f;
 
 public class SupportRaycast2 implements RaycastNarrowphase<Vector2f> {
-	private final Vector2f temp = new Vector2f();
+	private final Vector2f v1 = new Vector2f();
+	private Vector2f v2 = new Vector2f();
 
 	@Override
 	public boolean isColliding(SupportMap<Vector2f> Sa, Ray<Vector2f> ray) {
-		temp.set(-ray.getDirection().y, ray.getDirection().x);
-		float dot = dotRay(ray.getPosition(), Sa.getSupportCenter(), temp);
+		v1.set(-ray.getDirection().y, ray.getDirection().x);
+		float dot = dotRay(ray.getPosition(), Sa.getSupportCenter(), v1);
 		if (dot > 0) {
-			dot = dotRay(ray.getPosition(), Sa.supportPoint(temp), temp);
+			v2 = Sa.supportPoint(v1);
+			dot = dotRay(ray.getPosition(), v2, v1);
 			return dot <= 0;
 		} else if (dot < 0) {
-			dot = dotRay(ray.getPosition(), Sa.supportPointNegative(temp), temp);
-			return dot >= 0;
+			v1.negate();
+			v2 = Sa.supportPoint(v1);
+			dot = dotRay(ray.getPosition(), v2, v1);
+			return dot <= 0;
 		}
 
 		return true;
@@ -29,16 +34,19 @@ public class SupportRaycast2 implements RaycastNarrowphase<Vector2f> {
 	@Override
 	public float computeCollisionOnRay(SupportMap<Vector2f> Sa,
 			Ray<Vector2f> ray) {
-		// TODO Auto-generated method stub
-
-		// Take line from before, make triangle, refine iteratively, k?
-
+		// TODO
 		return 0;
 	}
 
 	@Override
 	public Vector2f computeCollision(SupportMap<Vector2f> Sa, Ray<Vector2f> ray) {
-		// TODO Auto-generated method stub
+		// Take line from before, make triangle, refine iteratively, k?
+		Vector2f bound1, bound2;
+		bound1 = Sa.supportPointNegative(ray.getDirection());
+		bound2 = v2;
+		System.out.println(bound1 + "; " + bound2);
+		System.out.println(dotRay(ray.getPosition(), bound1, v1));
+		System.out.println(dotRay(ray.getPosition(), bound2, v1));
 		return new Vector2f();
 	}
 
