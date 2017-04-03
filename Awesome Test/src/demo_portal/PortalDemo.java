1cc42389def907a5c113dade72de6219a0d490df
Oliver Schall <oliverschall22@googlemail.com>
2017-04-03 04:43:08 +0200

    Fixed demo portal placement (rotation)

--- Awesome Test/src/demo_portal/PortalDemo.java
+++ Awesome Test/src/demo_portal/PortalDemo.java
@@ -1,7 +1,5 @@
 package demo_portal;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import broadphase.DynamicAABBTree3;
@@ -11,7 +9,6 @@ import display.GLDisplay;
 import display.PixelFormat;
 import display.VideoSettings;
 import game.StandardGame;
-import gui.Color;
 import input.Input;
 import input.InputEvent;
 import input.KeyInput;
@@ -68,9 +65,6 @@ public class PortalDemo extends StandardGame {
 	final float PORTAL_HEIGHT = 0.001f;
 
 	Cylinder portal1, portal2;
-
-	List<Sphere> hitmarkers;
-	List<Line> hitnormals;
 
 	@Override
 	public void init() {
@@ -132,8 +126,8 @@ public class PortalDemo extends StandardGame {
 		inputs.addEvent(shootleft);
 		inputs.addEvent(shootright);
 
-		portal1 = new Cylinder(0, -10, 0, 1, PORTAL_HEIGHT, 32);
-		portal2 = new Cylinder(0, -10, 0, 1, PORTAL_HEIGHT, 32);
+		portal1 = new Cylinder(0, 9, 0, 1, PORTAL_HEIGHT, 32);
+		portal2 = new Cylinder(0, 9, 0, 1, PORTAL_HEIGHT, 32);
 		portal1.scale(1, 1, 2);
 		portal2.scale(1, 1, 2);
 
@@ -199,20 +193,6 @@ public class PortalDemo extends StandardGame {
 		hitnormalshader.addArgument("u_color", new Vector4f(0f, 0f, 1f, 1f));
 		addShader(hitmarkershader);
 		addShader(hitnormalshader);
-
-		hitmarkers = new ArrayList<Sphere>();
-		for (int i = 0; i < 6; i++) {
-			Sphere hitmarker = new Sphere(-100, -100, -100, 0.1f, 36, 36);
-			hitmarkershader.addObject(hitmarker);
-			hitmarkers.add(hitmarker);
-		}
-
-		hitnormals = new ArrayList<Line>();
-		for (int i = 0; i < 6; i++) {
-			Line hitnormal = new Line();
-			hitnormalshader.addObject(hitnormal);
-			hitnormals.add(hitnormal);
-		}
 	}
 
 	private void initLevel(Shader levelshader) {
@@ -227,7 +207,7 @@ public class PortalDemo extends StandardGame {
 		addBox(levelshader, 6, 2.5f, -6.5f, 2, 2.5f, 1.5f);
 		addBox(levelshader, 6.5f, -0.7f, 0, 1.5f, 3, 7).rotate(25, 0, 0);
 
-		Sphere testsphere = new Sphere(0, 2, 0, 2, 36, 36);
+		Sphere testsphere = new Sphere(0, 3, 0, 3, 36, 36);
 		testsphere.setRenderHints(false, true, false);
 		levelshader.addObject(testsphere);
 		RigidBody3 s = new RigidBody3(PhysicsShapeCreator.create(testsphere));
@@ -264,6 +244,10 @@ public class PortalDemo extends StandardGame {
 	final Vector3f down = new Vector3f(0, -1, 0);
 	final Vector3f up = new Vector3f(0, 1, 0);
 	final Vector3f jumpimpulse = new Vector3f(0, 5, 0);
+
+	Vector3f testvec = new Vector3f(0, 1, 0);
+	Quaternionf rotV = new Quaternionf();
+	Quaternionf rotH = new Quaternionf();
 
 	@Override
 	public void update(int delta) {
@@ -316,36 +300,6 @@ public class PortalDemo extends StandardGame {
 		if (shootright.isActive()) {
 			determinePortalPosition(portal2);
 		}
-
-		shootray.setPosition(cam.getTranslation());
-		shootray.setDirection(cam.getDirection());
-		int count = 0;
-		Set<RaycastResult<Vector3f>> hits = space.raycastAll(shootray);
-		for (RaycastResult<Vector3f> hit : hits) {
-			if (!hit.getHitObject().equals(playerbody)) {
-				Sphere hitsphere = hitmarkers.get(count);
-				hitsphere.translateTo(hit.getHitPosition());
-				hitsphere.setRendered(true);
-
-				Line hitnormal = hitnormals.get(count);
-				hitnormal.update(hit.getHitPosition(), VecMath.addition(hit.getHitPosition(), hit.getHitNormal()));
-				hitnormal.setRendered(true);
-
-				count++;
-			}
-		}
-
-		/*
-		 * shootray.setPosition(cam.getTranslation());
-		 * shootray.setDirection(cam.getDirection());
-		 * Set<Pair<RigidBody<Vector3f, ?, ?, ?>, Float>> hits =
-		 * space.raycastAllLambdas(shootray); float currentclosest =
-		 * Float.MAX_VALUE; for (Pair<RigidBody<Vector3f, ?, ?, ?>, Float> hit :
-		 * hits) { if(!hit.getFirst().equals(playerbody) && hit.getSecond() > 0
-		 * && hit.getSecond() < currentclosest) { currentclosest =
-		 * hit.getSecond(); } }
-		 * raytest.translateTo(shootray.pointOnRay(currentclosest));
-		 */
 	}
 
 	final Ray3 shootray = new Ray3(new Vector3f(), new Vector3f());
@@ -371,94 +325,38 @@ public class PortalDemo extends StandardGame {
 			// portal.rotateTo(playerbody.getRotation());
 			Vector3f hitnormal = closest.getHitNormal();
 
-			/*
-			 * Quaternion q; vector a = crossproduct(v1, v2) q.xyz = a; q.w =
-			 * sqrt((v1.Length ^ 2) * (v2.Length ^ 2)) + dotproduct(v1, v2)
-			 */
-
 			Quaternionf q = new Quaternionf();
 			portal.rotateTo(q);
 			Vector3f a = VecMath.crossproduct(up, hitnormal);
 			if (a.lengthSquared() > 0) {
-				Vector3f b = VecMath.crossproduct(hitnormal, a);
-				// q.rotate(90*VecMath.dotproduct(a, portalside), new
-				// Vector3f(0, 0, 1));
-				// q.rotate(90*VecMath.dotproduct(hitnormal, portalfront), new
-				// Vector3f(1, 0, 0));
+				Quaternionf rot = calculatePortalRotation(hitnormal);
+				portal.rotate(rot);
 
-				float anglex = 0;
-				float anglez = 0;
-				if (hitnormal.x > 0) {
-					anglez = angleBetweenVectors(hitnormal, new Vector3f(0, 0, 1));
-					if (hitnormal.z > 0) {
-						anglex = angleBetweenVectors(hitnormal, up);
-					} else {
-						anglex = angleBetweenVectors(hitnormal, up);
-					}
-				} else {
-					anglez = -angleBetweenVectors(hitnormal, new Vector3f(0, 0, 1));
-					if (hitnormal.z > 0) {
-						anglex = angleBetweenVectors(hitnormal, up);
-					} else {
-						anglex = angleBetweenVectors(hitnormal, up);
-					}
-				}
-				System.out.println(hitnormal + "; " + anglex + "; " + anglez);
-				// portal.rotate(90, 0, 0);
-				portal.rotate(0, anglez, 0);
-				portal.rotate(anglex, 0, 0);
-				// portal.rotate(rotationBetweenTwoVectors(QuatMath.transform(q,
-				// portalfront), hitnormal));
-				// System.out.println(90*VecMath.dotproduct(hitnormal,
-				// portalside) + "; " + 90*VecMath.dotproduct(hitnormal,
-				// portalfront));
-				q.normalize();
-				// portal.rotate(q);
 			} else {
 				portal.rotateTo(playerbody.getRotation());
 			}
-
-			// portalreference.set(QuatMath.transform(portal.getRotation(), new
-			// Vector3f(1, 0, 0)));
-			/*
-			 * Vector3f a = VecMath.crossproduct(portalreference, new
-			 * Vector3f(1, 0, 0)); System.out.println(hitnormal); if
-			 * (a.lengthSquared() > 0) { // a.normalize(); Quaternionf q = new
-			 * Quaternionf(); q.set(a.x, a.y, a.z, 1 +
-			 * VecMath.dotproduct(portalreference, hitnormal)); q.normalize();
-			 * portal.rotate(q); }
-			 */
 		}
 	}
 
-	private Quaternionf rotationBetweenTwoVectors(Vector3f from, Vector3f to) {
+	private Quaternionf calculatePortalRotation(Vector3f to) {
 		Quaternionf q = new Quaternionf();
-		Vector3f a = VecMath.crossproduct(from, to);
-		if (a.lengthSquared() > 0) {
-			q.set(a.x, a.y, a.z, 1 + VecMath.dotproduct(from, to));
-			q.normalize();
-		}
+
+		// for H project "to" onto x/z
+		// reference is front (0, 0, 1)
+		Vector3f a = new Vector3f(to.x, 0, to.z);
+		a.normalize();
+		float dot = VecMath.dotproduct(new Vector3f(0, 0, 1), a);
+		float angleH = (float) Math.toDegrees(Math.acos(dot));
+		if (to.x < 0)
+			angleH = -angleH;
+		q.rotate(angleH, new Vector3f(0, 1, 0));
+
+		// for V project "to" onto y
+		// reference is up (0, 1, 0)
+		dot = VecMath.dotproduct(new Vector2f(0, 1), new Vector2f(0, to.y));
+		float angleV = (float) Math.toDegrees(Math.acos(dot));
+		q.rotate(angleV, new Vector3f(1, 0, 0));
+
 		return q;
-	}
-
-	private float angleBetweenVectors(Vector3f a, Vector3f b) {
-		return (float) Math.toDegrees(Math.acos(VecMath.dotproduct(a, b)));
-	}
-
-	private class Line extends ShapedObject3 {
-		Color c;
-
-		public Line() {
-			setRenderMode(GLConstants.LINES);
-			c = Color.CYAN;
-		}
-
-		public void update(Vector3f start, Vector3f end) {
-			delete();
-			addVertex(start, c);
-			addVertex(end, c);
-			addIndices(0, 1);
-			prerender();
-		}
 	}
 }
