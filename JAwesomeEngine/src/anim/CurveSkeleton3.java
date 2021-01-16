c30464a53894e87144da1b69311f91e12a008d5c
Oliver Schall <oliverschall22@googlemail.com>
2021-01-16 23:16:14 +0100

    Added mirroring option to CurveSkeleton3

--- JAwesomeEngine/src/anim/CurveSkeleton3.java
+++ JAwesomeEngine/src/anim/CurveSkeleton3.java
@@ -1,13 +1,19 @@
 package anim;
 
-import math.QuatMath;
-import math.VecMath;
+import curves.BezierCurve3;
+import curves.SimpleAngularCurvePath;
+import curves.SimpleCurvePath;
+import curves.SquadCurve3;
 import objects.BaseObject;
 import objects.BaseObject3;
 import quaternion.Quaternionf;
 import vector.Vector3f;
 
 public class CurveSkeleton3 extends CurveSkeleton<Vector3f, Quaternionf> {
+	boolean mirroredX = false;
+	boolean mirroredY = false;
+	boolean mirroredZ = false;
+	boolean invertRotation = false;
 
 	public CurveSkeleton3() {
 		super(null, new BaseObject3[0]);
@@ -30,27 +36,98 @@ public class CurveSkeleton3 extends CurveSkeleton<Vector3f, Quaternionf> {
 
 	@Override
 	public void updateAnimation(float animationTimer) {
+		CurveAnimation<Vector3f, Quaternionf> currentAnimation = (dynamicAnimationTransition != null)
+				? dynamicAnimationTransition.getAnimation()
+				: animation;
 		for (int i = 0; i < bodyparts.size(); i++) {
 			BaseObject<Vector3f, Quaternionf> part = bodyparts.get(i);
-			Vector3f point = animation.getAnimationTranslationPath(i).getPoint(animationTimer);
-			if(attachedTo != null) {
-				point.transform(attachedTo.getRotation());
-			}
-			part.translate(point);
-			part.rotate(animation.getAnimationRotationPath(i).getRotation(animationTimer));
+			Vector3f trans = currentAnimation.getAnimationTranslationPath(i).getPoint(animationTimer);
+			if (mirroredX)
+				trans.x = -trans.x;
+			if (mirroredY)
+				trans.y = -trans.y;
+			if (mirroredZ)
+				trans.z = -trans.z;
+			if(attachedTo != null)
+				trans.transform(attachedTo.getRotation());
+			part.translate(trans);
+			Quaternionf rot = currentAnimation.getAnimationRotationPath(i).getRotation(animationTimer);
+			if (invertRotation)
+				rot.invert();
+			part.rotate(rot);
 		}
 	}
-
+	
+	// TODO: Optimize!
 	public void setDynamicAnimation(CurveAnimation<Vector3f, Quaternionf> animationparam, float dynamicAnimationSpeed) {
-		this.animation = new CurveAnimation<Vector3f, Quaternionf>(animationparam);
+		dynamicAnimationTransition = new DynamicCurveAnimationTransition<Vector3f, Quaternionf>(animationparam,
+				dynamicAnimationSpeed);
 		for (int i = 0; i < bodyparts.size(); i++) {
-			BaseObject<Vector3f, Quaternionf> bodypart = bodyparts.get(i);
-			animation.getAnimationTranslationPath(i).getCurves().get(0)
-					.setStartPoint(VecMath.subtraction(bodypart.getTranslation(), attachedTo.getTranslation()));
-			animation.getAnimationRotationPath(i).getCurves().get(0)
-					.setStartRotation(QuatMath.substraction(bodypart.getRotation(), attachedTo.getRotation()));
+			Vector3f trans = animation.getAnimationTranslationPath(i).getPoint(animationTimer);
+			SimpleCurvePath<Vector3f> translationpath = new SimpleCurvePath<Vector3f>(
+					dynamicAnimationTransition.getAnimation().getAnimationTranslationPath(i));
+			SimpleAngularCurvePath<Quaternionf> rotationpath = new SimpleAngularCurvePath<Quaternionf>(
+					dynamicAnimationTransition.getAnimation().getAnimationRotationPath(i));
+			BezierCurve3 oldcurve = (BezierCurve3) translationpath.getCurves().get(0);
+			SquadCurve3 oldrotcurve = (SquadCurve3) rotationpath.getCurves().get(0);
+			translationpath.getCurves().set(0,
+					new BezierCurve3(trans, oldcurve.getP1(), oldcurve.getP2(), oldcurve.getP3()));
+			// TODO: fix, when squadcurves are properly fixed.
+			Quaternionf newSquadrotation = animation.getAnimationRotationPath(i).getRotation(animationTimer);
+			rotationpath.getCurves().set(0,
+					new SquadCurve3(newSquadrotation, newSquadrotation, oldrotcurve.getR2(), oldrotcurve.getR3()));
+			dynamicAnimationTransition.getAnimation().getAnimationTranslationPaths().set(i, translationpath);
+			dynamicAnimationTransition.getAnimation().getAnimationRotationPaths().set(i, rotationpath);
 		}
+		animation = animationparam;
 		animationTimer = 0;
 	}
+	
+	public void mirrorX() {
+		mirroredX = !mirroredX;
+	}
 
+	public boolean isMirroredX() {
+		return mirroredX;
+	}
+
+	public void setMirroredX(boolean mirroredX) {
+		this.mirroredX = mirroredX;
+	}
+
+	public void mirrorY() {
+		mirroredY = !mirroredY;
+	}
+
+	public boolean isMirroredY() {
+		return mirroredY;
+	}
+
+	public void setMirroredY(boolean mirroredY) {
+		this.mirroredY = mirroredY;
+	}
+	
+	public void mirrorZ() {
+		mirroredZ = !mirroredZ;
+	}
+
+	public boolean isMirroredZ() {
+		return mirroredZ;
+	}
+
+	public void setMirroredZ(boolean mirroredZ) {
+		this.mirroredZ = mirroredZ;
+	}
+
+	public void invertRotation() {
+		invertRotation = !invertRotation;
+	}
+
+	public boolean isRotationInverted() {
+		return invertRotation;
+	}
+
+	public void setRotationInverted(boolean invertRotation) {
+		this.invertRotation = invertRotation;
+	}
 }
